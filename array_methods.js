c059fb335d2fa6c35e9715deb7ea57bdda2f835c
Treagan Birbal <treaganbirbal@pursuit.org>
2018-10-15 22:03:31 -0400

    Array_Methods

---
+++ array_methods.js
@@ -0,0 +1,19 @@
+// . Using Array.prototype add a new method `myForEach` that mimics the behaivor of the built in `forEach`.
+//
+// Array.prototype.myForEach = function(callback) {
+//   for (let i = 0; i < this.length; i++){
+//     callback(this[i]);
+//   }
+// };
+// [1, 2, 3].myForEach((el) => {
+// console.log (el)
+// })
+//
+// Array.prototype.myMap = function(callback){
+//    this.myForEach(el => {
+
+})
+// }
+// console.log (myMap)
+
+console.log(Array)
